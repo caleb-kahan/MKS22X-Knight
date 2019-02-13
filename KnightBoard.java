9e908cfbea87e47a67a73dcbf6f6f6214bfd1acf
Caleb Kahan <ckahan00@stuy.edu>
2019-02-13 14:34:19 -0500

    Argument Exception +Initialization

---
+++ KnightBoard.java
@@ -0,0 +1,20 @@
+public class KnightBoard{
+    private int [][] board;
+    public KnightBoard(int startingRows, int startingCols){
+      if(startingCols <=0 || startingRows<=0)throw IllegalArgumentException("A parameter is less than 1");
+      board = new int [startingRows][startingCols];
+    }
+    /*public String toString(){
+      String formatter = "";
+      String [] values = new int[board.length*board[0].length];
+      for(int [] row: board){
+        for(int value: row){
+          values+=value;
+          formatter += "%2d ";
+        }
+        formatter +="%n";
+      }
+      return String.format(formatter,(Object[])values);
+    }*/
+
+}
