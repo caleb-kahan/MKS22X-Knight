4843e9846bba3219ad0ec8d1768c060325d7d5dd
Caleb Kahan <ckahan00@stuy.edu>
2019-02-23 02:41:58 +0000

    0 errors, will it workgit add .!

--- MoverV1.java
+++ MoverV1.java
@@ -11,12 +11,12 @@ public class MoverV1{
   public static ArrayList<MoverV1> pos (){
     ArrayList<MoverV1> pos = new ArrayList<MoverV1>();
     for(int i=0;i<2;i++){
-      int hoz = (k%2==0) ? 2 : 1;
+      int hoz = (i%2==0) ? 2 : 1;
       int vert = (hoz==2) ? 1 : 2;
       for(int j=0;j<2;j++){
         hoz*= (j%2==0) ? 1 : -1;
         for(int k=0;k<2;k++){
-          vert*= (j%2==0) ? 1 : -1;
+          vert*= (k%2==0) ? 1 : -1;
           pos.add(new MoverV1 (hoz, vert));
         }
       }
