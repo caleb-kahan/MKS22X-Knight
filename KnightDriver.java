c1ead1f399c4d0bc42f17a354fa973131908eb42
Caleb Kahan <ckahan00@stuy.edu>
2019-02-24 12:31:20 +0000

    now my thing can go to 63X63, yay

--- KnightDriver.java
+++ KnightDriver.java
@@ -1,8 +1,8 @@
 public class KnightDriver{
     public static void main(String [] args){
-      KnightBoard board = new KnightBoard(5,6);
-      System.out.println(board.countSolutions(0,0));
-      //System.out.println(board.solve(0,0)); 
+      KnightBoard board = new KnightBoard(63,63);
+      //System.out.println(board.countSolutions(0,0));
+      System.out.println(board.solve(0,0)); 
       //System.out.println(board); 
 	
     }
